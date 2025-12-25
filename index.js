bd48ab0bd6f0818c8d44473dc2c07f1732a3f7b1
lilu <lilu@bigdatacd.com>
2025-12-25 14:35:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const puppeteer = require("puppeteer");
+const fun = require("./inject.js");
+
+const LOGINAME = "13880615404";
+const PASSWORD = "Liao199212";
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true, // true = 无头模式（默认）
+        protocolTimeout: 0,
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 1280,
+        height: 600,
+    });
+
+    await page.goto(
+        "https://basic.sc.smartedu.cn/ThirdPortalService/user/otherlogin!login.ac?appkey=C56DA16ECBC56FBEEC908DA09E45C72C917A80118F057FA1F0B5BAE41CC9CC9DECD5BDB7133FE17C328C5D37B37CA8E7&pkey=5D79CA42E45C5273DF8532D09E1F158B15E25919CDB958940F84D5E63F5F53A1ECD5BDB7133FE17C328C5D37B37CA8E7&params=718F83A5347CBFDB7D1A9065FA090FE949D92330BB9A3351FE0715C5B8A3E86F37916C1004E835C7C7F964E3F301477F7D37F04485FA8707845DAAA23356236ED1D326CF5A5E3C263470516EE9B4A2ED",
+        {
+            waitUntil: "networkidle2",
+        }
+    );
+
+    await page.waitForSelector(".submit-btn");
+
+    await page.type("#loginName", LOGINAME);
+    await page.type("#password", PASSWORD);
+
+    await page.click(".submit-btn");
+
+    const isLogin = await Promise.race([
+        page.waitForNavigation().then(() => true),
+        (async () => {
+            const tips = await page.waitForSelector(".layui-layer-btn0", {
+                timeout: 5000,
+            });
+            const text = await page.evaluate((el) => el.innerText, tips);
+            if (text === "确定") {
+                await page.click(".layui-layer-btn1");
+                await page.waitForNavigation();
+                return true;
+            }
+            return false;
+        })(),
+    ]);
+
+    console.log("isLogin", isLogin);
+    let userName = Date.now();
+    if (isLogin) {
+        await page.waitForSelector(".courseList", {
+            visible: true,
+            timeout: 15000,
+        });
+
+        await page.exposeFunction("showMsg", (msg) => {
+            console.log(msg);
+        });
+
+        userName = await page.evaluate(
+            () => document.querySelector("#link > div").innerText
+        );
+
+        console.log("用户名：", userName);
+        await page.evaluate(fun.start);
+
+        await page.goto(
+            "https://basic.sc.smartedu.cn/hd/teacherTraining/myTrain",
+            {
+                waitUntil: "networkidle2",
+            }
+        );
+        await page.waitForSelector(".courseList", {
+            visible: true,
+            timeout: 15000,
+        });
+    }
+
+    await page.screenshot({ path: `${userName}.png` });
+
+    await browser.close();
+})();
---
+++ inject.js
@@ -0,0 +1,139 @@
+module.exports.start = async function () {
+    Object.keys(window).forEach((key) => {
+        if (key.startsWith("webpackChunk")) {
+            console.log(key);
+            window[key].push([
+                [99999],
+                {},
+                (r) => {
+                    window.wr = r;
+                },
+            ]);
+        }
+    });
+    // 用来挂全局的函数
+    function exposeLibs(runtime) {
+        if (!runtime) return;
+        const exportsMap = {};
+        for (const id in runtime.m) {
+            let exp = runtime(id);
+
+            const candidate = exp.default || exp;
+
+            if (candidate && candidate.version) {
+                console.log("candidate", id, exp);
+            }
+
+            // 判断 axios
+            if (
+                !exportsMap.axios &&
+                candidate &&
+                candidate.get &&
+                candidate.post &&
+                candidate.defaults
+            ) {
+                exportsMap.axios = candidate;
+            }
+
+            // 判断 Vue
+            if (
+                !exportsMap.Vue &&
+                candidate &&
+                candidate.version &&
+                candidate.component &&
+                candidate.directive
+            ) {
+                exportsMap.Vue = candidate;
+            }
+
+            // 判断 ElementUI
+            if (
+                !exportsMap.ElementUI &&
+                candidate &&
+                candidate.install &&
+                candidate.version &&
+                candidate.Button
+            ) {
+                exportsMap.ElementUI = candidate;
+            }
+
+            // 提前结束判断
+            if (exportsMap.axios && exportsMap.Vue && exportsMap.ElementUI)
+                break;
+        }
+
+        // 挂到全局
+        for (const key in exportsMap) {
+            window[key] = exportsMap[key];
+            console.log(`✅ ${key} 已挂到 window`);
+        }
+
+        return exportsMap;
+    }
+
+    // 执行
+    exposeLibs(window.wr);
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    async function getCourseDetails(param) {
+        const res = await axios({
+            url: "/studyCourse/getCourseDetails?courseId=" + param,
+            method: "get",
+        });
+        return res.returnData;
+    }
+
+    async function recordStudyProcess(param) {
+        const res = await axios({
+            url: "/studyCourseUser/recordProcess",
+            method: "post",
+            data: param,
+        });
+        return res.returnData;
+    }
+
+    async function study(courseId) {
+        const loading = window.Vue.prototype.$loading({
+            target: document.querySelector("body"),
+            lock: true,
+            text: `正在自动学习...`,
+            // background: "rgba(0, 0, 0, 0.7)",
+        });
+        const courseInfo = await getCourseDetails(courseId);
+        const chapters = courseInfo.chapters;
+        for (const [chapterIndex, chapter] of chapters.entries()) {
+            for (const [
+                sectionIndex,
+                section,
+            ] of chapter.studySubsections.entries()) {
+                const { courseId, chapterId, id: subsectionId } = section;
+                const { state, videoTime } = section.studyFiles;
+
+                loading.text = `正在自动学习：[${courseId}]的${chapterIndex + 1}-${
+                    sectionIndex + 1
+                    }`;
+                showMsg(loading.text)
+                if (state != "3") {
+                    let recordValue = 0;
+                    while (recordValue != 1) {
+                        await delay(1000 * 30);
+                        recordValue = await recordStudyProcess({
+                            courseId: courseId, //课程ID
+                            chapterId: chapterId, //章节ID
+                            subsectionId: subsectionId, // 子节点ID
+                            studyTime: parseInt(videoTime), //观看时间 parseInt(videoTime) + 1
+                            state: "2", // 当前状态
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    const courseList = ["1983723370145034240", "1983474287572594688"];
+
+    for (const courseId of courseList) {
+        await study(courseId);
+    }
+};
